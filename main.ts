c1b2de915c684477c25851fac17ddb3c45e4839d
bryan weinstein <bryanweinstein@outlook.com>
2021-03-31 15:58:41 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,27 @@
+input.onButtonPressed(Button.A, function () {
+    servos.P0.setAngle(0)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # # # # #
+        `)
+    basic.pause(2000)
+    servos.P0.stop()
+})
+input.onButtonPressed(Button.B, function () {
+    servos.P0.setAngle(180)
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        # # # # #
+        `)
+    basic.pause(2000)
+    servos.P0.stop()
+})
 basic.forever(function () {
-// read the potentiometer with P0 connected to middle and 
-// red connected to right side pin
-    basic.showNumber(pins.analogReadPin(AnalogPin.P0))
+    pins.digitalWritePin(DigitalPin.P2, 1)
 })
